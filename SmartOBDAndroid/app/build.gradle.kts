4ecd368fbd364d300692fe10fd0436f5a1735701
abeytaneh-dotcom <abeytaneh@gmail.com>
2026-09-21 00:14:32 +0330

    Add Smart OBD Android: SmartOBDAndroid/app/build.gradle.kts

---
+++ SmartOBDAndroid/app/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins { id("com.android.application") }
+android {
+    namespace = "ir.khanehremap.smartobd"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "ir.khanehremap.smartobd"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
