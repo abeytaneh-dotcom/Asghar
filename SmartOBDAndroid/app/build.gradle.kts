55e5048870a6f235a8e6182303c56f3f97c729fa
abeytaneh-dotcom <abeytaneh@gmail.com>
2026-09-21 07:42:09 +0330

    Use stable update signature and bump app version

--- SmartOBDAndroid/app/build.gradle.kts
+++ SmartOBDAndroid/app/build.gradle.kts
@@ -2,11 +2,26 @@ plugins { id("com.android.application") }
 android {
     namespace = "ir.khanehremap.smartobd"
     compileSdk = 35
+    signingConfigs {
+        create("stable") {
+            storeFile = file("../signing/smartobd-release.jks")
+            storePassword = "KhanehRemap2026!"
+            keyAlias = "smartobd"
+            keyPassword = "KhanehRemap2026!"
+        }
+    }
     defaultConfig {
         applicationId = "ir.khanehremap.smartobd"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
+    }
+    buildTypes {
+        getByName("debug") { signingConfig = signingConfigs.getByName("stable") }
+        getByName("release") {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("stable")
+        }
     }
 }
