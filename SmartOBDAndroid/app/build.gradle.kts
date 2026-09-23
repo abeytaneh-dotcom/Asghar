cc4beace51bca43a676f2b82c78bbaa2e086d0d3
abeytaneh-dotcom <abeytaneh@gmail.com>
2026-09-23 19:00:58 +0330

    Bump Smart OBD Pro serial build to 2.4.1

--- SmartOBDAndroid/app/build.gradle.kts
+++ SmartOBDAndroid/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ir.khanehremap.smartobd"
         minSdk = 23
         targetSdk = 35
-        versionCode = 6
-        versionName = "2.4.0"
+        versionCode = 7
+        versionName = "2.4.1"
     }
     buildTypes {
         getByName("debug") { signingConfig = signingConfigs.getByName("stable") }
