34e4616dad7452e4058b77937bca561274f4bb44
abeytaneh-dotcom <abeytaneh@gmail.com>
2026-09-25 17:33:28 +0330

    Add Iranian-market vehicle and ECU catalog

---
+++ SmartOBD-Diagnostic-Pro/android/app/src/main/assets/catalog.js
@@ -0,0 +1,137 @@
+window.KR_CATALOG = [
+ {group:'ایرانی',name:'ایران خودرو',key:'ikco',mark:'IK',models:[
+  {name:'پژو 206 فرانسوی',year:'2001–2011',engine:'TU3 / TU5',ecus:['J34','J34P','Sagem S2000','Bosch ME7.4.4','Bosch ME7.4.5']},
+  {name:'پژو 206 ایرانی',year:'2012–2026',engine:'TU3 / TU5',ecus:['Valeo J34P','Bosch ME7.4.4','Bosch ME7.4.5','Bosch ME7.4.9','Continental']},
+  {name:'پژو 207i',year:'2010–2026',engine:'TU5 / TU5P',ecus:['Bosch ME7.4.4','Bosch ME7.4.5','Bosch ME7.4.9','Valeo J34P','Continental EMS']},
+  {name:'پژو 405',year:'1992–2020',engine:'XU7 / TU5',ecus:['Sagem S2000','Valeo J34','Valeo J34P','Bosch ME7.4.4','Siemens']},
+  {name:'پژو پارس',year:'2000–2026',engine:'XU7 / TU5 / XU7P',ecus:['Sagem S2000','Valeo J34','Valeo J34P','Bosch ME7.4.4','Bosch ME7.4.9','Continental']},
+  {name:'سمند LX',year:'2001–2021',engine:'XU7 / EF7',ecus:['Sagem S2000','Valeo J34','Bosch ME7.4.4','Bosch ME7.4.9','Siemens/Continental']},
+  {name:'سمند سورن',year:'2007–2024',engine:'EF7',ecus:['Bosch ME7.4.9','Continental','Siemens','Bosch ME17']},
+  {name:'دنا',year:'2014–2026',engine:'EF7',ecus:['Bosch ME7.4.9','Continental','Bosch ME17','Siemens']},
+  {name:'دنا پلاس',year:'2017–2026',engine:'EF7 / EF7 TC',ecus:['Bosch ME7.4.9','Bosch MED17','Continental','Bosch ME17']},
+  {name:'رانا / رانا پلاس',year:'2012–2026',engine:'TU5 / TU5P',ecus:['Bosch ME7.4.4','Bosch ME7.4.5','Bosch ME7.4.9','Valeo J34P','Continental']},
+  {name:'تارا',year:'2021–2026',engine:'TU5P',ecus:['Bosch ME17','Continental EMS','Valeo/Continental CAN']},
+  {name:'ری را',year:'2024–2026',engine:'EF7P TC',ecus:['Bosch MED17','Continental EMS']},
+  {name:'پژو 2008',year:'2017–2020',engine:'EP6 Turbo',ecus:['Bosch MED17.4','Continental']},
+  {name:'هایما S5',year:'2017–2026',engine:'1.5T',ecus:['Bosch ME17','Delphi MT80','Continental']},
+  {name:'هایما S7',year:'2015–2026',engine:'2.0 / 1.8T',ecus:['Bosch ME7.9.7','Bosch ME17','Delphi MT80']},
+  {name:'هایما S8',year:'2023–2026',engine:'1.6T',ecus:['Bosch MED17','Continental']},
+  {name:'آریسان 1 / 2',year:'2015–2026',engine:'XU7 / XU7P',ecus:['Sagem S2000','Valeo J34','Bosch ME7.4.9','Continental']}
+ ]},
+ {group:'ایرانی',name:'سایپا',key:'saipa',mark:'SA',models:[
+  {name:'پراید',year:'1993–2021',engine:'M13',ecus:['Sagem S2000','Siemens','Valeo','Bosch ME7.4.4','SSAT']},
+  {name:'تیبا',year:'2009–2022',engine:'M15',ecus:['Siemens/Continental','Valeo','Bosch ME17','SSAT']},
+  {name:'ساینا',year:'2015–2026',engine:'M15',ecus:['Siemens/Continental','Bosch ME17','SSAT']},
+  {name:'کوییک',year:'2017–2026',engine:'M15',ecus:['Siemens/Continental','Bosch ME17','SSAT']},
+  {name:'شاهین',year:'2020–2026',engine:'M15 TC',ecus:['Bosch ME17','Continental EMS']},
+  {name:'اطلس',year:'2023–2026',engine:'M15GSI',ecus:['Bosch ME17','Continental EMS']},
+  {name:'وانت 151',year:'2014–2026',engine:'M13',ecus:['Siemens','Valeo','SSAT']}
+ ]},
+ {group:'ایرانی',name:'پارس خودرو',key:'pars',mark:'PK',models:[
+  {name:'تندر 90 / L90',year:'2007–2019',engine:'K4M',ecus:['Siemens EMS3132','Continental EMS3132','Sagem S3000']},
+  {name:'ساندرو',year:'2015–2019',engine:'K4M',ecus:['Continental EMS3132','Siemens EMS3132']},
+  {name:'مگان',year:'2008–2013',engine:'K4M / F4R',ecus:['Siemens EMS3132','Sagem S3000','Continental']},
+  {name:'ماکسیما',year:'2002–2012',engine:'VQ30',ecus:['Hitachi','Nissan ECCS']},
+  {name:'برلیانس H220/H230',year:'2014–2020',engine:'1.5',ecus:['Bosch ME7.9.7','Delphi MT80']},
+  {name:'برلیانس H320/H330',year:'2014–2020',engine:'1.5 / 1.65',ecus:['Bosch ME7.9.7','Delphi MT80']}
+ ]},
+ {group:'مونتاژی / چینی',name:'مدیران خودرو (MVM)',key:'mvm',mark:'MVM',models:[
+  {name:'MVM 110',year:'2005–2016',engine:'0.8 / 1.1',ecus:['Bosch ME7.9.7','Delphi MT20U']},
+  {name:'MVM 315',year:'2011–2020',engine:'1.5',ecus:['Bosch ME7.9.7','Delphi MT22U']},
+  {name:'MVM X22',year:'2016–2026',engine:'1.5',ecus:['Delphi MT22U','Bosch ME17','Continental']},
+  {name:'MVM X33',year:'2010–2025',engine:'2.0 / 1.5T',ecus:['Bosch ME7.9.7','Delphi MT80','Bosch ME17']},
+  {name:'MVM X55',year:'2020–2026',engine:'1.5T',ecus:['Bosch ME17','Continental']},
+  {name:'آریزو 5',year:'2016–2026',engine:'1.5 / 1.5T',ecus:['Bosch ME17','Delphi MT80','Continental']},
+  {name:'آریزو 6',year:'2019–2026',engine:'1.5T',ecus:['Bosch ME17','Continental']},
+  {name:'تیگو 5',year:'2015–2021',engine:'2.0',ecus:['Bosch ME7.9.7','Delphi MT80']},
+  {name:'تیگو 7 / 8',year:'2017–2026',engine:'1.5T / 1.6T',ecus:['Bosch ME17','Bosch MED17','Continental']}
+ ]},
+ {group:'مونتاژی / چینی',name:'کرمان موتور',key:'kerman',mark:'KMC',models:[
+  {name:'JAC J5',year:'2013–2018',engine:'1.5 / 1.8',ecus:['Bosch ME7.9.7','Delphi MT80']},
+  {name:'JAC J4',year:'2018–2026',engine:'1.5',ecus:['Bosch ME17','Delphi MT80']},
+  {name:'JAC S3',year:'2017–2026',engine:'1.6',ecus:['Bosch ME17','Delphi MT80']},
+  {name:'JAC S5',year:'2015–2023',engine:'2.0T / 1.5T',ecus:['Bosch MED17','Delphi MT80','Continental']},
+  {name:'KMC J7',year:'2022–2026',engine:'1.5T',ecus:['Bosch MED17','Continental']},
+  {name:'KMC X5',year:'2023–2026',engine:'1.5T',ecus:['Bosch MED17','Continental']},
+  {name:'KMC T8',year:'2020–2026',engine:'2.0T',ecus:['Bosch MED17','Delphi']},
+  {name:'KMC T9',year:'2023–2026',engine:'2.0T',ecus:['Bosch MED17','Continental']}
+ ]},
+ {group:'ایرانی / مونتاژی',name:'بهمن موتور',key:'bahman',mark:'BM',models:[
+  {name:'مزدا 3',year:'2007–2019',engine:'2.0',ecus:['Denso','Mitsubishi Electric']},
+  {name:'B30',year:'2017–2021',engine:'1.6',ecus:['Bosch ME17','Continental']},
+  {name:'فیدلیتی',year:'2021–2026',engine:'1.5T',ecus:['Bosch ME17','Continental']},
+  {name:'دیگنیتی',year:'2021–2026',engine:'1.5T / 2.0T',ecus:['Bosch ME17','Bosch MED17','Continental']},
+  {name:'ریسپکت',year:'2022–2026',engine:'1.5T',ecus:['Bosch ME17','Continental']},
+  {name:'کاپرا',year:'2010–2026',engine:'2.4',ecus:['Delphi MT20U','Bosch ME7.9.7']}
+ ]},
+ {group:'ایرانی / مونتاژی',name:'فردا موتور',key:'farda',mark:'FMC',models:[
+  {name:'FMC 511',year:'2021–2024',engine:'1.5',ecus:['Bosch ME17','Delphi MT80']},
+  {name:'FMC SX5',year:'2020–2025',engine:'1.6',ecus:['Bosch ME17','Delphi']},
+  {name:'FMC T5',year:'2021–2026',engine:'1.5T',ecus:['Bosch ME17','Continental']}
+ ]},
+ {group:'فرانسوی',name:'رنو',key:'renault',mark:'R',models:[
+  {name:'L90 / Logan',year:'2007–2019',engine:'K4M',ecus:['Siemens EMS3132','Continental EMS3132','Sagem S3000']},
+  {name:'ساندرو',year:'2015–2019',engine:'K4M',ecus:['Continental EMS3132','Siemens EMS3132']},
+  {name:'مگان',year:'2008–2013',engine:'K4M / F4R',ecus:['Siemens EMS3132','Sagem S3000']},
+  {name:'داستر',year:'2014–2018',engine:'F4R',ecus:['Continental EMS','Sagem']},
+  {name:'فلوئنس',year:'2014–2017',engine:'2.0',ecus:['Continental EMS','Hitachi']},
+  {name:'کپچر',year:'2016–2018',engine:'1.2T',ecus:['Continental EMS','Bosch MED17']}
+ ]},
+ {group:'فرانسوی',name:'سیتروئن',key:'citroen',mark:'C',models:[
+  {name:'زانتیا',year:'2001–2010',engine:'1.8 / 2.0',ecus:['Bosch MP7.3','Sagem SL96','Bosch ME7.4.4']},
+  {name:'C3',year:'2018–2020',engine:'1.6T',ecus:['Bosch MED17.4','Continental']},
+  {name:'C5',year:'2008–2012',engine:'2.0',ecus:['Bosch ME7.4.5','Valeo']}
+ ]},
+ {group:'کره‌ای',name:'هیوندای',key:'hyundai',mark:'H',models:[
+  {name:'اکسنت',year:'2011–2018',engine:'1.6',ecus:['Kefico','Bosch ME17.9.11']},
+  {name:'النترا',year:'2011–2020',engine:'1.6 / 1.8 / 2.0',ecus:['Kefico','Bosch ME17.9.11','SIM2K']},
+  {name:'سوناتا',year:'2006–2020',engine:'2.0 / 2.4',ecus:['Kefico','SIM2K','Bosch ME17.9.11']},
+  {name:'توسان',year:'2007–2020',engine:'2.0 / 2.4',ecus:['Kefico','SIM2K']},
+  {name:'سانتافه',year:'2007–2018',engine:'2.4 / 2.7',ecus:['Kefico','SIM2K']},
+  {name:'آزرا / گرنجور',year:'2007–2018',engine:'2.4 / 3.0 / 3.3',ecus:['Kefico','SIM2K']},
+  {name:'i20 / i30',year:'2010–2018',engine:'1.4 / 1.6',ecus:['Kefico','Bosch ME17.9.11']}
+ ]},
+ {group:'کره‌ای',name:'کیا',key:'kia',mark:'KIA',models:[
+  {name:'ریو',year:'2005–2017',engine:'1.5 / 1.6',ecus:['Kefico','Bosch ME17.9.11']},
+  {name:'سراتو',year:'2008–2018',engine:'1.6 / 2.0',ecus:['Kefico','SIM2K','Bosch ME17.9.11']},
+  {name:'اسپورتیج',year:'2008–2018',engine:'2.0 / 2.4',ecus:['Kefico','SIM2K']},
+  {name:'سورنتو',year:'2008–2018',engine:'2.4 / 3.3',ecus:['Kefico','SIM2K']},
+  {name:'اپتیما',year:'2010–2018',engine:'2.0 / 2.4',ecus:['Kefico','SIM2K']},
+  {name:'پیکانتو',year:'2012–2017',engine:'1.2',ecus:['Kefico','Bosch ME17.9.11']}
+ ]},
+ {group:'چینی',name:'جیلی',key:'geely',mark:'G',models:[
+  {name:'امگرند EC7',year:'2013–2017',engine:'1.8',ecus:['Bosch ME7.9.7','Delphi MT80']},
+  {name:'X7',year:'2014–2017',engine:'2.4',ecus:['Bosch ME7.9.7','Delphi MT80']}
+ ]},
+ {group:'چینی',name:'لیفان',key:'lifan',mark:'L',models:[
+  {name:'520',year:'2008–2013',engine:'1.3 / 1.6',ecus:['Delphi MT20U','Bosch ME7.9.7']},
+  {name:'620',year:'2011–2016',engine:'1.6 / 1.8',ecus:['Bosch ME7.9.7','Delphi MT22U']},
+  {name:'X50',year:'2015–2018',engine:'1.5',ecus:['Bosch ME7.9.7','Delphi MT22U']},
+  {name:'X60',year:'2013–2018',engine:'1.8',ecus:['Bosch ME7.9.7','Delphi MT80']},
+  {name:'820',year:'2016–2018',engine:'2.4',ecus:['Bosch ME17','Delphi MT80']}
+ ]},
+ {group:'ژاپنی',name:'لکسوس',key:'lexus',mark:'Lx',models:[
+  {name:'IS',year:'2007–2018',engine:'2.5 / 3.0',ecus:['Denso']},{name:'ES',year:'2007–2020',engine:'2.5 / 3.5',ecus:['Denso']},
+  {name:'GS',year:'2007–2017',engine:'3.0 / 3.5',ecus:['Denso']},{name:'NX',year:'2015–2020',engine:'2.0T / 2.5H',ecus:['Denso']},
+  {name:'RX',year:'2007–2020',engine:'3.5',ecus:['Denso']},{name:'LX',year:'2008–2020',engine:'5.7',ecus:['Denso']}
+ ]},
+ {group:'ژاپنی',name:'تویوتا',key:'toyota',mark:'T',models:[
+  {name:'کرولا',year:'2005–2020',engine:'1.6 / 1.8',ecus:['Denso']},{name:'کمری',year:'2005–2020',engine:'2.4 / 2.5',ecus:['Denso']},
+  {name:'پرادو',year:'2005–2018',engine:'2.7 / 4.0',ecus:['Denso']},{name:'RAV4',year:'2013–2020',engine:'2.5',ecus:['Denso']},
+  {name:'یاریس',year:'2007–2017',engine:'1.3 / 1.5',ecus:['Denso']}
+ ]},
+ {group:'ژاپنی',name:'نیسان',key:'nissan',mark:'N',models:[
+  {name:'ماکسیما',year:'2002–2012',engine:'3.0',ecus:['Hitachi','Nissan ECCS']},{name:'تیانا',year:'2010–2013',engine:'2.5',ecus:['Hitachi']},
+  {name:'مورانو',year:'2008–2012',engine:'3.5',ecus:['Hitachi']},{name:'قشقایی',year:'2009–2013',engine:'2.0',ecus:['Hitachi']}
+ ]},
+ {group:'ژاپنی',name:'مزدا',key:'mazda',mark:'MZ',models:[
+  {name:'مزدا 3',year:'2007–2019',engine:'1.6 / 2.0',ecus:['Denso','Mitsubishi Electric']},{name:'مزدا 2',year:'2011–2013',engine:'1.5',ecus:['Denso']}
+ ]},
+ {group:'ژاپنی',name:'میتسوبیشی',key:'mitsubishi',mark:'MI',models:[
+  {name:'لنسر',year:'2014–2018',engine:'1.6 / 1.8',ecus:['Mitsubishi Electric']},{name:'ASX',year:'2014–2018',engine:'2.0',ecus:['Mitsubishi Electric']},
+  {name:'اوتلندر',year:'2014–2018',engine:'2.4',ecus:['Mitsubishi Electric']}
+ ]},
+ {group:'ژاپنی',name:'سوزوکی',key:'suzuki',mark:'S',models:[
+  {name:'ویتارا',year:'2007–2019',engine:'2.0 / 2.4',ecus:['Denso','Mitsubishi Electric']}
+ ]}
+];
